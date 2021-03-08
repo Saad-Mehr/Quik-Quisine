16604e205c493ba485b3e18f1f83e7fbb24343e2
richaurd-valencia <70352631+richaurd-valencia@users.noreply.github.com>
2021-03-07 21:43:04 -0800

    Add files via upload

--- lib/recipes.dart
+++ lib/recipes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'search.dart';
 import 'package:http/http.dart' as http;
+import 'package:quikquisine490/user.dart';
 import 'dart:convert';
 import 'dart:async';
+import 'recipedetails.dart';
 
 var resultSearchTerm;
 List<dynamic> resultNames;
@@ -13,16 +15,16 @@ List<dynamic> sortedTotalRecipeIng = [];
 List<dynamic> filteredSortedTotal = [];
 List<dynamic> mealPlannerRecipes = new List();
 Map<String, String> get headers => {
-  "X-User-Email": 'test@gmail.com',
-  "X-User-Token":'KV7aSwEfxti-X2Mr6LxJ',
+  "X-User-Email": UserList[0],
+  "X-User-Token": UserList[2],
   'Content-Type': 'application/json; charset=UTF-8',
 };
 
 
 Future<List> addToList(int id) async{
-  var url = 'https://quik-quisine.herokuapp.com/api/v1/users/users/51/meal_planner_baskets';
+  var url = 'https://quik-quisine.herokuapp.com/api/v1/users/users/'+UserList[3].toString()+'/meal_planner_baskets';
   var body = {};
-  body["user_id"] ="51";
+  body["user_id"] =UserList[3].toString();
   body["recipe_id"]=id;
   var jsonbody = {};
   jsonbody["user"] = body;
@@ -33,7 +35,7 @@ Future<List> addToList(int id) async{
 
 Future retrieveList() async{
 
-  http.Response response = await http.get('https://quik-quisine.herokuapp.com/api/v1/users/users/51/meal_planner_baskets', headers: headers);
+  http.Response response = await http.get('https://quik-quisine.herokuapp.com/api/v1/users/users/'+UserList[3].toString()+'/meal_planner_baskets', headers: headers);
   var parsedJson = jsonDecode(response.body);
   var data = parsedJson['data'];
   mealPlannerRecipes = data;
@@ -75,8 +77,10 @@ Future getRecipes() async{
   await sortAllIngredients();
 }
 
-Future _ackAlert(BuildContext context,Widget thumbnail, String title, String subtitle, String instructions,String serving,String ingredients, int id) {
-
+Future _ackAlert(BuildContext context,Widget thumbnail, String title, String subtitle, String instructions,String serving,String ingredients,double AverageRating, String review, int id) {
+if(review == null){
+  review = 'No reviews yet.';
+}
   return showDialog(
     context: context,
     builder: (BuildContext context) {
@@ -86,7 +90,15 @@ Future _ackAlert(BuildContext context,Widget thumbnail, String title, String sub
         content: SingleChildScrollView(
           child: ListBody(
             children: <Widget>[
-              Text(title + "\n" + subtitle + "\n" + serving + "\n\nIngredients:\n" + ingredients + '\nInstructions:\n' + instructions)
+              Text(title + "\n" + subtitle + "\n" + serving + "\nIngredients:\n" + ingredients + '\nInstructions:\n' + instructions +'\nRating:'),
+              SmoothStarRating(
+                allowHalfRating: true,
+                starCount: 5,
+                rating: AverageRating,
+                size: 20,
+                color: Colors.amber,
+                borderColor: Colors.black,
+              ),
             ],
           ),
         ),
@@ -99,6 +111,17 @@ Future _ackAlert(BuildContext context,Widget thumbnail, String title, String sub
               Navigator.of(context).pop();
             },
           ),
+          FlatButton(
+            child: Text('More Details'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => new recipedetails(thumbnail,title,subtitle,instructions,serving,ingredients,id,AverageRating,review),
+                ),
+              );
+            },
+          )
         ],
       );
     },
@@ -219,35 +242,17 @@ class ResultsWidget extends StatelessWidget {
 
 List<Widget> getRecipeResults() {
 
-  if(isIngredientSearch == true) {
-    for (int i = 0; i < recipeNames.length; i++) {
-      recipesResultsList.add(CustomListItemTwo(
-        thumbnail: Container(
-          child: Image.network(recipePicURLs[i], fit: BoxFit.fill,),
-        ),
-        title: recipeNames[i],
-        subtitle: 'Description: ${recipeDesc[i]}',
-        serving: 'Serving size: ${recipeServing[i]}',
-        ingredients: ' ${filteredSortedIng[i]}\n'
-            'Missing ingredients:\n ${missingIngredients[i].toString()
-            .replaceAll("[", "").replaceAll("]", "")
-            .replaceAll(",", "\n")}\n',
-        instructions: '${recipePrep[i]}',
-      ));
-    }
-  } else {
-    for (int i = 0; i < recipeNames.length; i++) {
-      recipesResultsList.add(CustomListItemTwo(
-        thumbnail: Container(
-          child: Image.network(recipePicURLs[i], fit: BoxFit.fill,),
-        ),
-        title: recipeNames[i],
-        subtitle: 'Description: ${recipeDesc[i]}',
-        serving: 'Serving size: ${recipeServing[i]}',
-        ingredients: ' ${filteredSortedIng[i]}',
-        instructions: '${recipePrep[i]}',
-      ));
-    }
+  for (int i = 0; i < recipeNames.length; i++) {
+    recipesResultsList.add(CustomListItemTwo(
+      thumbnail: Container(
+        child: Image.network(recipePicURLs[i], fit: BoxFit.fill,),
+      ),
+      title: recipeNames[i],
+      subtitle: 'Description: ${recipeDesc[i]}',
+      serving: 'Serving size: ${recipeServing[i]}',
+      ingredients: ' ${filteredSortedIng[i]}',
+      instructions: '${recipePrep[i]}',
+    ));
   }
 
   if(resultNames.length == 0) {
@@ -281,7 +286,8 @@ class _ArticleDescription extends StatelessWidget {
     this.instructions,
     this.serving,
     this.ingredients,
-    this.id
+    this.id,
+    this.AverageRating
   }) : super(key: key);
 
   final String title;
@@ -290,9 +296,12 @@ class _ArticleDescription extends StatelessWidget {
   final String serving;
   final String ingredients;
   final String id;
+  double AverageRating;
 
   @override
   Widget build(BuildContext context) {
+    if(AverageRating == null)
+      AverageRating = 0;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -330,6 +339,15 @@ class _ArticleDescription extends StatelessWidget {
                 ),
               ),
               const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+              SmoothStarRating(
+                allowHalfRating: true,
+                starCount: 5,
+                rating: AverageRating,
+                size: 20,
+                color: Colors.amber,
+                borderColor: Colors.black,
+
+              ),
              /* Text(
                 '$ingredients',
                 maxLines: 1,
@@ -365,6 +383,8 @@ class CustomListItemTwo extends StatelessWidget {
     this.ingredients,
     this.instructions,
     this.id,
+    this.AverageRating,
+    this.review
   }) : super(key: key);
 
   final Widget thumbnail;
@@ -373,16 +393,20 @@ class CustomListItemTwo extends StatelessWidget {
   final String serving;
   final String ingredients;
   final String instructions;
+   double AverageRating;
+  final String review;
 
   final int id;
 
 
   @override
   Widget build(BuildContext context) {
+    if(AverageRating==null)
+      AverageRating = 0;
     return GestureDetector(
       onTap: () {
 
-        _ackAlert(context,this.thumbnail, this.title, this.subtitle, this.instructions,this.serving,this.ingredients, this.id);
+        _ackAlert(context,this.thumbnail, this.title, this.subtitle, this.instructions,this.serving,this.ingredients,this.AverageRating, this.review, this.id);
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10.0),
@@ -404,6 +428,7 @@ class CustomListItemTwo extends StatelessWidget {
                     subtitle: subtitle,
                     serving: serving,
                     ingredients: ingredients,
+                    AverageRating: AverageRating,
                   ),
 
                 ),
@@ -424,6 +449,8 @@ class CustomListItemThree extends StatelessWidget {
     this.subtitle,
     this.serving,
     this.instructions,
+    this.AverageRating,
+    this.review,
     this.id,
   }) : super(key: key);
 
@@ -432,6 +459,8 @@ class CustomListItemThree extends StatelessWidget {
   final String subtitle;
   final String serving;
   final String instructions;
+  final double AverageRating;
+  final String review;
   final int id;
 
   @override
@@ -460,6 +489,7 @@ class CustomListItemThree extends StatelessWidget {
                     subtitle: subtitle,
                     serving: serving,
                     instructions: instructions,
+                    AverageRating: AverageRating,
                   ),
                 ),
               )
@@ -488,6 +518,8 @@ class MyStatelessWidget extends StatelessWidget {
           ingredients: " ${filteredSortedTotal[index]}",
           instructions: "${totalRecipes[index]['preparation']}",
           id: totalRecipes[index]['id'],
+          AverageRating: double.parse(totalRecipes[index]['AverageRating']),
+          review: totalRecipes[index]['review'],
         );
       },
     );
@@ -512,10 +544,88 @@ class MyMealPlanner extends StatelessWidget {
           serving: "Servings: ${mealPlannerRecipes[index]['serving']}",
           instructions: "${mealPlannerRecipes[index]['preparation']}",
           id: mealPlannerRecipes[index]['id'],
+          AverageRating: double.parse(totalRecipes[index]['AverageRating']),
+          review: totalRecipes[index]['review'],
         );
       },
     );
 
+  }
+}
+
+typedef void RatingChangeCallback(double rating);
+class SmoothStarRating extends StatelessWidget {
+  final int starCount;
+  final double rating;
+  final RatingChangeCallback onRatingChanged;
+  final Color color;
+  final Color borderColor;
+  final double size;
+  final bool allowHalfRating;
+  final double spacing;
+  SmoothStarRating(
+      {this.starCount = 5,
+        this.rating = 0.0,
+        this.onRatingChanged,
+        this.color,
+        this.borderColor,
+        this.size,
+        this.spacing = 0.0,
+        this.allowHalfRating = true}) {
+    assert(this.rating != null);
+  }
+  Widget buildStar(BuildContext context, int index) {
+    Icon icon;
+    if (index >= rating) {
+      icon = new Icon(
+        Icons.star_border,
+        color: borderColor ?? Theme.of(context).primaryColor,
+        size: size ?? 25.0,
+      );
+    } else if (index > rating - (allowHalfRating ? 0.5 : 1.0) &&
+        index < rating) {
+      icon = new Icon(
+        Icons.star_half,
+        color: color ?? Theme.of(context).primaryColor,
+        size: size ?? 25.0,
+      );
+    } else {
+      icon = new Icon(
+        Icons.star,
+        color: color ?? Theme.of(context).primaryColor,
+        size: size ?? 25.0,
+      );
+    }
+    return new GestureDetector(
+      onTap: () {
+        if (this.onRatingChanged != null) onRatingChanged(index + 1.0);
+      },
+      onHorizontalDragUpdate: (dragDetails) {
+        RenderBox box = context.findRenderObject();
+        var _pos = box.globalToLocal(dragDetails.globalPosition);
+        var i = _pos.dx / size;
+        var newRating = allowHalfRating ? i : i.round().toDouble();
+        if (newRating > starCount) {
+          newRating = starCount.toDouble();
+        }
+        if (newRating < 0) {
+          newRating = 0.0;
+        }
+        if (this.onRatingChanged != null) onRatingChanged(newRating);
+      },
+      child: icon,
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.transparent,
+      child: new Wrap(
+          alignment: WrapAlignment.start,
+          spacing: spacing,
+          children: new List.generate(
+              starCount, (index) => buildStar(context, index))),
+    );
   }
 }
 
